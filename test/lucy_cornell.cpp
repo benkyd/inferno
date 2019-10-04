771d3330f6cd4e15e358f7e422c09b3c18fcc182
Ben <benjaminkyd@gmail.com>
2019-10-04 17:25:29 +0100

    And awwwaaayyyyy we gooooo

--- test/lucy_cornell.cpp
+++ test/lucy_cornell.cpp
@@ -2,8 +2,8 @@
 
 #include "../src/inferno.hpp"
 
-static const int width = 600;
-static const int height = 600;
+static const int width = 2000;
+static const int height = 2000;
 
 int main(int argc, char** argv) {
 	InfernoEngine inferno;
@@ -19,16 +19,16 @@ int main(int argc, char** argv) {
 	scene->sky = sky;
 
 	Material* mat = new GlossyMaterial({ 1.0f, 1.0f, 1.0f }, 0.2f, fastDegreetoRadian(30.0f));
-	//std::vector<Triangle*> tris = LoadTrianglesBasic("E://Projects//Inferno//resources//models//dragon-cornell-size.obj", "E://Projects//Inferno//resources//models", mat);
-	std::vector<Triangle*> tris = LoadTrianglesBasic("/home/ben/programming/inferno/resources/models/lucy.obj", "/home/ben/programming/inferno/resources/models/", mat);
+	std::vector<Triangle*> tris = LoadTrianglesBasic("E://Projects//Inferno//resources//models//lucy.obj", "E://Projects//Inferno//resources//models//", mat);
+	//std::vector<Triangle*> tris = LoadTrianglesBasic("/home/ben/programming/inferno/resources/models/lucy.obj", "/home/ben/programming/inferno/resources/models/", mat);
 
 	Mesh* mesh = new Mesh(tris);
 	mesh->Translate({ -0.05f, -1.01, -3.8f });
 	mesh->Optimise();
 	scene->meshs.push_back(mesh);
 
-	// std::vector<Triangle*> tris1 = LoadTrianglesBasic("E://Projects//Inferno//resources//models//cornell-box.obj", "E://Projects//Inferno//resources//models//");
-	std::vector<Triangle*> tris1 = LoadTrianglesBasic("/home/ben/programming/inferno/resources/models/cornell-box.obj", "/home/ben/programming/inferno/resources/models/");
+	std::vector<Triangle*> tris1 = LoadTrianglesBasic("E://Projects//Inferno//resources//models//cornell-box.obj", "E://Projects//Inferno//resources//models//");
+	//std::vector<Triangle*> tris1 = LoadTrianglesBasic("/home/ben/programming/inferno/resources/models/cornell-box.obj", "/home/ben/programming/inferno/resources/models/");
 
 	Mesh* mesh1 = new Mesh(tris1);
 	mesh1->Translate({ 0.01f, -1.0, -3.6f });
